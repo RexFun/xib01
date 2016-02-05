152c9ede9453a9afd8547e51d04fca558cfa80b7
RexFun <oleFun@icloud.com>
2016-02-05 10:14:16 +0800

    Initial Commit

---
+++ xib01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xib01
+//
+//  Created by mac373 on 16/2/5.
+//  Copyright © 2016年 ole. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xib01/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  xib01
+//
+//  Created by mac373 on 16/2/5.
+//  Copyright © 2016年 ole. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
